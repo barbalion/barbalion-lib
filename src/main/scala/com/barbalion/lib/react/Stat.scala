89aebbbda37c99a7f6333230b37424ab716d4f82
Alexander Ermakov <barbalion@gmail.com>
2017-01-05 00:30:22 +0300

    Stat collection during calculations

---
+++ src/main/scala/com/barbalion/lib/react/Stat.scala
@@ -0,0 +1,24 @@
+package com.barbalion.lib.react
+
+trait Stat extends LazyCalculator {
+  object stat {
+    var totalSet = 0
+    var totalFirstRead = 0
+    var totalReCalc = 0
+  }
+
+  override def valueSet(r: Reactive[_]): Unit = {
+    stat.totalSet += 1
+    super.valueSet(r)
+  }
+
+  override def valueFirstRead(r: Reactive[_]): Unit = {
+    stat.totalFirstRead += 1
+    super.valueFirstRead(r)
+  }
+
+  override def needReCalc(r: Reactive[_]): Unit = {
+    stat.totalReCalc += 1
+    super.needReCalc(r)
+  }
+}
