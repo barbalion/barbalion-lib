4648f614ee4850c34f69734331ee38b63e0d3c6b
Alexander Ermakov <barbalion@gmail.com>
2017-01-16 14:28:40 +0300

    Type annotation

--- src/main/scala/com/barbalion/lib/math/VectorE.scala
+++ src/main/scala/com/barbalion/lib/math/VectorE.scala
@@ -7,18 +7,18 @@ import com.barbalion.lib.math.DoubleE.Zero
   */
 abstract class VectorE[T <: VectorE[_]](val coordinates: List[DoubleE]) {
   // squared length
-  lazy val r2 = coordinates map {
+  lazy val r2: DoubleE = coordinates map {
     _.sqr
   } sum Zero
 
   // vector length
-  lazy val r = r2.sqrt
+  lazy val r: DoubleE = r2.sqrt
 
-  def +(a: VectorE[T]) = combineVectors(a, Zero.plus)
+  def +(a: VectorE[T]): VectorE[T] = combineVectors(a, Zero.plus)
 
-  def -(a: VectorE[T]) = combineVectors(a, Zero.minus)
+  def -(a: VectorE[T]): VectorE[T] = combineVectors(a, Zero.minus)
 
-  protected def combineVectors(a: VectorE[T], op: (DoubleE, DoubleE) => DoubleE) = buildInstance(
+  protected def combineVectors(a: VectorE[T], op: (DoubleE, DoubleE) => DoubleE): VectorE[T] = buildInstance(
     (coordinates, a.coordinates).zipped map op
   )
 
@@ -26,37 +26,37 @@ abstract class VectorE[T <: VectorE[_]](val coordinates: List[DoubleE]) {
 }
 
 case class Vector2E(x: DoubleE, y: DoubleE) extends VectorE(List(x, y)) {
-  override def buildInstance(coordinates: List[DoubleE]) = coordinates match {
+  override def buildInstance(coordinates: List[DoubleE]): Vector2E = coordinates match {
     case x_ :: y_ :: Nil => Vector2E(x_, y_)
     case _ => throw new Exception("Bad vector.")
   }
 
-  def +(a: Vector2E) = combineVectors(a, Zero.plus).asInstanceOf[Vector2E]
+  def +(a: Vector2E): Vector2E = combineVectors(a, Zero.plus).asInstanceOf[Vector2E]
 
-  def -(a: Vector2E) = combineVectors(a, Zero.minus).asInstanceOf[Vector2E]
+  def -(a: Vector2E): Vector2E = combineVectors(a, Zero.minus).asInstanceOf[Vector2E]
 
   def /(div: DoubleE) = Vector2E(x / div, y / div)
 
   def *(div: DoubleE) = Vector2E(x * div, y * div)
 
-  def ==(a: Vector2E) = x.value == a.x.value && y.value == a.y.value
+  def ==(a: Vector2E): Boolean = x.value == a.x.value && y.value == a.y.value
 
   /**
     * returns the vector of the same direction and length = 1.0
     */
-  lazy val normalize = /(r)
+  lazy val normalize: Vector2E = /(r)
 
-  def posE = (x, y)
+  def posE: (DoubleE, DoubleE) = (x, y)
 
-  def pos = (x.value, y.value)
+  def pos: (Double, Double) = (x.value, y.value)
 
-  def posFloat = (x.value.toFloat, y.value.toFloat)
+  def posFloat: (Float, Float) = (x.value.toFloat, y.value.toFloat)
 
   def reverse = Vector2E(-x, -y)
 
-  def phi = math.atan2(y.value, x.value)
+  def phi: Double = math.atan2(y.value, x.value)
 
-  def isGoodVector = x.isGoodNumber && y.isGoodNumber
+  def isGoodVector: Boolean = x.isGoodNumber && y.isGoodNumber
 }
 
 object Vector2E {
@@ -73,13 +73,13 @@ object Vector2E {
 }
 
 case class Vector3E(x: DoubleE, y: DoubleE, z: DoubleE) extends VectorE(List(x, y, z)) {
-  override def buildInstance(coordinates: List[DoubleE]) = coordinates match {
+  override def buildInstance(coordinates: List[DoubleE]): Vector3E = coordinates match {
     case x_ :: y_ :: z_ :: Nil => Vector3E(x_, y_, z_)
     case _ => throw new Exception("Bad vector.")
   }
 
-  def pos = (x, y, z)
+  def pos: (DoubleE, DoubleE, DoubleE) = (x, y, z)
 
-  def posFloat = (x.value.toFloat, y.value.toFloat, z.value.toFloat)
+  def posFloat: (Float, Float, Float) = (x.value.toFloat, y.value.toFloat, z.value.toFloat)
 }
 
